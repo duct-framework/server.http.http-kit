ddf0d1e2633569557f799772b0aee615cb55f33c
James Reeves <jreeves@weavejester.com>
2017-06-13 17:39:58 +0100

    Add URL and description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/server.http.http-kit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Integrant methods for running a HTTP-Kit web server"
+  :url "https://github.com/duct-framework/server.http.http-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]

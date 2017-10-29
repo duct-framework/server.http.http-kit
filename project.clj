d654ffe2c7ffca19bce572d0c0e47a4bfcf5825a
James Reeves <jreeves@weavejester.com>
2017-10-29 18:34:08 +0000

    Update Clojure version to 1.9.0-beta3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/server.http.http-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [duct/core "0.6.1"]
                  [http-kit "2.2.0"]
                  [duct/logger "0.2.1"]

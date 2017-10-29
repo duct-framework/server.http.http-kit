2373a34ee8886670b27a81e6f63f83e56fc6ffdc
James Reeves <jreeves@weavejester.com>
2017-10-29 18:25:58 +0000

    Update duct/logger to 0.2.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [duct/core "0.6.1"]
                  [http-kit "2.2.0"]
-                 [duct/logger "0.1.1"]
+                 [duct/logger "0.2.1"]
                  [integrant "0.6.1"]]
   :profiles
   {:dev {:dependencies [[clj-http "2.1.0"]]}})

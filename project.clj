db390d673efef18c5ba6ff26189c25d1431f6e65
James Reeves <jreeves@weavejester.com>
2019-04-03 22:59:35 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/duct-framework/server.http.http-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [duct/core "0.6.2"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [duct/core "0.7.0"]
                  [http-kit "2.3.0"]
-                 [duct/logger "0.2.1"]
-                 [integrant "0.6.3"]]
+                 [duct/logger "0.3.0"]
+                 [integrant "0.7.0"]]
   :profiles
   {:dev {:dependencies [[clj-http "3.9.1"]]}})

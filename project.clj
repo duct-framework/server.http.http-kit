78b99aade8633b692aa8ee2da97caba55c8abe5d
James Reeves <jreeves@weavejester.com>
2019-04-03 23:01:03 +0100

    Release 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/server.http.http-kit "0.1.3"
+(defproject duct/server.http.http-kit "0.1.4"
   :description "Integrant methods for running a HTTP-Kit web server"
   :url "https://github.com/duct-framework/server.http.http-kit"
   :license {:name "Eclipse Public License"
